1ad1e549b276062ec487a43fe07694a47e781ed5
Ikka <daniel.ikka@edu.ece.fr>
2019-10-07 11:48:54 +0200

    initial commit on homePage

---
+++ Web/Header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav id="nav-bar-header">
+        <ul class="row">
+            <li class="list"><a href="#">Home</a></li>
+            <li class="list"><a href="#">Experiences</a></li>
+            <li class="list"><a href="#">Projects</a></li>
+            <li class="list"><a href="#">Education</a></li>
+        </ul>
+    </nav>
+</header>
--- Web/Index.php
+++ Web/Index.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <title>Daniel IKKA</title>
+    <link href="../Stylesheets/General.css" type="text/css" rel="stylesheet" >
+    <link href="../Stylesheets/NavStylesheet.css" type="text/css" rel="stylesheet">
+</head>
 
+<body>
 
+    <!-- Include the page's header -->
+    <?php include("Header.php"); ?>
+
+    <main>
+
+    </main>
+
+</body>
+
+</html>
