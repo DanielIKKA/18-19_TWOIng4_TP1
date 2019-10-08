55e203d13434226ee55f20c4910a19f7883db69f
Ikka <daniel.ikka@edu.ece.fr>
2019-10-08 22:13:10 +0200

    - picture and first section done - css management

--- Web/Index.php
+++ Web/Index.php
@@ -4,18 +4,68 @@
 <head>
     <meta charset="UTF-8">
     <title>Daniel IKKA</title>
-    <link href="../Stylesheets/General.css" type="text/css" rel="stylesheet" >
+
+    <link href="../Stylesheets/General.css" type="text/css" rel="stylesheet">
     <link href="../Stylesheets/NavStylesheet.css" type="text/css" rel="stylesheet">
+    <link href="../Stylesheets/Index.css" type="text/css" rel="stylesheet">
+
 </head>
 
 <body>
 
     <!-- Include the page's header -->
-    <?php include("Header.php"); ?>
+    <!--<?php //include("Header.php"); ?>-->
 
-    <main>
+    <main class="vRatio1">
+        <section id="first_section" class="column vRatio5">
 
+            <img src="../Assets/Images/_MG_2989.png" class="vRatio6 rounded_image contain" alt="profile-pic">
+
+            <div id="text_wrapper">
+                <h1 class="semi-bold tSize4">Daniel IKKA</h1>
+                <p class="regular tSize2">Ecole Centrale d'Electronique - Ece PARIS</p>
+                <p class="extra-light tSize2">Engineering School</p>
+            </div>
+
+            <div id="icon_wrapper">
+                <svg width="22px" height="65px" viewBox="0 0 18 54" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g id="A3-Copy" transform="translate(-411.000000, -310.000000)">
+                            <g id="Section" transform="translate(227.000000, 8.000000)">
+                                <g id="Icon" transform="translate(184.000000, 302.000000)">
+                                    <g id="Mouse">
+                                        <g>
+                                            <path d="M9,0.5 C4.07970076,0.5 0.5,3.94711925 0.5,10 L0.5,20 C0.5,26.0528807 4.07970076,29.5 9,29.5 C13.9202992,29.5 17.5,26.0528807 17.5,20 L17.5,10 C17.5,3.94711925 13.9202992,0.5 9,0.5 Z" id="Path" stroke="#1A3244"></path>
+                                            <path d="M9,4.5 L9,11.5" id="Line" stroke="#D89D95" stroke-linecap="round" stroke-linejoin="round"></path>
+                                        </g>
+                                    </g>
+                                    <g id="Arrows" transform="translate(2.000000, 39.000000)" stroke="#D89D95" stroke-linecap="round" stroke-linejoin="round">
+                                        <polyline id="Line-2" stroke-opacity="0.597383086" points="0 4 7 10 14 4"></polyline>
+                                        <polyline id="Line-2-Copy-3" stroke-opacity="0.9" points="0 8 7 14 14 8"></polyline>
+                                        <polyline id="Line-2-Copy" stroke-opacity="0.303731425" points="0 0 7 6 14 3.39728246e-14"></polyline>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+            </div>
+
+        </section>
+
+        <section id="second_section" class="column vRatio1 relative">
+            <img src="../Assets/Images/img1.png" alt="BG_pic" class="vRatio1 cover">
+
+            <div id="text_work_exp">
+                <p id="title_work_exp">
+                    Work <span>Experiences</span>
+                </p>
+            </div>
+        </section>
     </main>
+
+
+
 
 </body>
 
