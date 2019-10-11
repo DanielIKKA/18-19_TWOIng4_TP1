bb336cc20d36ff382959f517ba515a4223d78e0a
Ikka <daniel.ikka@edu.ece.fr>
2019-10-11 22:36:36 +0200

    update new animation navBar fix animatino section's title on home page

--- JavaScript/NavBar.js
+++ JavaScript/NavBar.js
@@ -2,56 +2,25 @@ let $window = $(window);
 
 function navBarManage() {
 
-    let $firstSection = $("#first_section");
-    let $navBar = $('#nav-bar-header');
+    let $navBar = $('#header_wrapper');
 
-    let beginPoint = $firstSection.outerHeight() / 3;
-
-    let scrollTop = $window.scrollTop();
+    let range = $navBar.outerHeight();
 
 
-    if(scrollTop <= beginPoint) {
+    if(window.pageYOffset <= range*8) {
         $navBar.css({
-            'opacity' : 0,
+            'top' : $window.scrollTop()/8-range + 'px'
         });
     }
-
-    if (scrollTop <= 100) { // hide nav 100px before begin point to have a better fluency effect
-        $navBar.hide();
-    }
-
-    // manage opacity
-    if (scrollTop >= beginPoint) {
-        $navBar.show();
-        $navBar.css({ 'opacity' : 1 });
-    }
-
 }
 
 $(document).ready(function () {
 
-    let $navBar = $('#nav-bar-header');
+    let $navBar = $('#header_wrapper');
 
-    let oldOpacity = 0;
-
-    // $navBar.hover(function () {
-    //     if(window.pageYOffset > 100) {
-    //         oldOpacity = $(this).css("opacity");
-    //         $(this).css({
-    //             'opacity' : 0.6
-    //         });
-    //     }
-    // }, function () {
-    //     if(window.pageYOffset > 100) {
-    //         $(this).css({
-    //             'opacity': oldOpacity
-    //         });
-    //     } else {
-    //         $(this).css({
-    //             'opacity': 0
-    //         });
-    //     }
-    // });
+    $navBar.css({
+       'top' : -$navBar.outerHeight() + 'px'
+    });
 });
 
 $window.on('scroll', function () {
--- Web/Header.php
+++ Web/Header.php
@@ -1,5 +1,5 @@
-<header>
-    <nav id="nav-bar-header" class="w1 row no_margin no_padding">
+<header id="header_wrapper" class="w1 row">
+    <nav id="nav-bar-header" class="w5 row no_margin no_padding">
         <a class="list w1 h1" href="#">Home</a>
         <a class="list w1 h1" href="#">Experiences</a>
         <a class="list w1 h1" href="#">Projects</a>
